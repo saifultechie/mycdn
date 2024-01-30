8116daeff2220a3397a9daecc3c07eff5992144c
islam.saiful <islam.saiful@brainstation-23.com>
2024-01-30 17:26:42 +0600

    div added into my root div

--- feedbackLibrary.js
+++ feedbackLibrary.js
@@ -5,4 +5,14 @@
         }
     }
     window.myLibrary = myLibrary
+
+    let myDiv = document.createElement("div")
+
+    myDiv.style.width = "300px"
+    myDiv.style.height = "300px"
+    myDiv.style.backgroundColor = "red"
+
+    let rootDiv = document.querySelector("#root")
+    rootDiv.appendChild(myDiv)
+
 })()
