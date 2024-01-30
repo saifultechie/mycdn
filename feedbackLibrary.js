482c7d5b442f7c35a510c06e1b4abcdd51dbf9e6
islam.saiful <islam.saiful@brainstation-23.com>
2024-01-30 17:37:44 +0600

    div added into my root div

--- feedbackLibrary.js
+++ feedbackLibrary.js
@@ -2,20 +2,19 @@
     let myLibrary = {
         greet : function(name){
             console.log("hello mr ", name)
+        },
+        addDiv : function(){
+            let myDiv = document.createElement("div")
+
+            myDiv.style.width = "300px"
+            myDiv.style.height = "300px"
+            myDiv.style.backgroundColor = "red"
+        
+            let rootDiv = document.querySelector("#root")
+            console.log(rootDiv, "root div")
+            rootDiv.appendChild(myDiv)
         }
     }
     window.myLibrary = myLibrary
-
-    console.log("ok my library")
-
-    let myDiv = document.createElement("div")
-
-    myDiv.style.width = "300px"
-    myDiv.style.height = "300px"
-    myDiv.style.backgroundColor = "red"
-
-    let rootDiv = document.querySelector("#root")
-    console.log(rootDiv, "root div")
-    rootDiv.appendChild(myDiv)
 
 })()
